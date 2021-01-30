ab3e3a748c482e4d52d8c0f537fa09fd99d9e662
aratius <arata1129matsu@icloud.com>
2021-01-31 00:13:21 +0900

    コメント追加

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const http = require('http')  //httpというnode.js組み込みのモジュール
 const fs = require('fs')
 
+let request;
+let response;
+
 //フロントJSでいう非同期関数的な感じかな
 //req: クライアントからサーバーへのリクエスト情報が詰まってるオブジェクト
 //res: サーバーからクライアントへの返信に関するオブジェクト
-var server = http.createServer((req, res) => {
-fs.readFile('./index.html', 'UTF-8', (err, data) => {
-  res.writeHead(300, {'Content-type': 'text/html'})
-  res.write(data)
-  res.end()
-})
-});
+var server = http.createServer(getFromClient);
 server.listen(3000)
 console.log('Server started');
+
+function getFromClient(req, res){
+  request = req
+  response = res
+  fs.readFile('./index.html', 'UTF-8', writeToResponse)
+  console.log('1');
+}
+
+function writeToResponse(err, data){
+  response.writeHead(300, {'Content-type': 'text/html'})
+  response.write(data)
+  response.end()
+  console.log('new client connected');
+}
