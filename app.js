ec234e06808a0745386c208c59cc05c899e9a890
aratius <arata1129matsu@icloud.com>
2021-01-30 23:43:40 +0900

    fsモジュールで外部html読み込み

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const http = require('http')  //httpというnode.js組み込みのモジュール
+const fs = require('fs')
 
 //フロントJSでいう非同期関数的な感じかな
 //req: クライアントからサーバーへのリクエスト情報が詰まってるオブジェクト
 //res: サーバーからクライアントへの返信に関するオブジェクト
 var server = http.createServer((req, res) => {
-  res.end('<html><body><h1>こんにちは</h1><p>welcome to my site</p></body></html>')
+fs.readFile('./index.html', 'UTF-8', (err, data) => {
+  res.writeHead(300, {'Content-type': 'text/html'})
+  res.write(data)
+  res.end()
+})
 });
 server.listen(3000)
+console.log('Server started');
