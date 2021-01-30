2676516ff4d2d1cd56365fb4ffc7dbda9c55f4ba
aratius <arata1129matsu@icloud.com>
2021-01-30 23:32:59 +0900

    インラインhtml

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http')  //httpというnode.js組み込みのモジュール
 
 //フロントJSでいう非同期関数的な感じかな
+//req: クライアントからサーバーへのリクエスト情報が詰まってるオブジェクト
+//res: サーバーからクライアントへの返信に関するオブジェクト
 var server = http.createServer((req, res) => {
-  res.end('Hello Node.js')
+  res.end('<html><body><h1>hello</h1><p>welcome to my site</p></body></html>')
 });
 server.listen(3000)
