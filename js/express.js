8f62927a99981ad87bf9a613c8fdab3a638677e1
aratius <arata1129matsu@icloud.com>
2021-01-31 16:08:27 +0900

    自作のmiddleware

--- js/express.js
+++ js/express.js
@@ -1,14 +1,13 @@
 const express = require('express')
-const logger = require('morgan')
 const app = express()
 
 //middleware functionの例
-// const a_middleware_function = function(req, res, next) {
-//   console.log('called middleware');
-//   next()
-// }
+const a_middleware_function = function(req, res, next) {
+  console.log('called middleware');
+  next()
+}
 
-app.use(logger('dev'))
+app.use(a_middleware_function)
 
 app.get('/', function(req, res) {
   res.send('Hello ARATA')
